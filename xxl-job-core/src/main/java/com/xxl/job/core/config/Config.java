b26f77795032b4342f5ac851a911533ea927755a
L-border <964097833@qq.com>
2021-06-08 16:35:16 +0800

    增加注释和docker-compose文件

--- xxl-job-core/src/main/java/com/xxl/job/core/config/Config.java
+++
@@ -1,9 +0,0 @@
-package com.xxl.job.core.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = {"com.xxl.job.core.controller"})
-public class Config {
-}
